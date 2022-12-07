e9bb89d1acc74003a3683b8c0d7b2993a4a57ebf
flemay <flemay@gmail.com>
2022-12-07 14:07:44 +0100

    MySQL connectée et fonctionnelle.

--- src/main/java/com/gymargente/gymmanager/AppMySQL.java
+++ src/main/java/com/gymargente/gymmanager/AppMySQL.java
@@ -1,17 +1,46 @@
 package com.gymargente.gymmanager;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AppMySQL {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.jdbc"); // juste pour tester.
+        int[] ids = {0,1,2};
+        String[] names = {"Sue", "Bob", "Charley"};
 
-//        String dbUrl = "jdbc:sqlite:people.db";
-//        Connection conn = DriverManager.getConnection(dbUrl);
-//        conn.setAutoCommit(false); // autocommit : met à jour la db automatiquement
+        Class.forName("com.mysql.cj.jdbc.Driver"); // juste pour tester.
+
+        String dbUrl = "jdbc:mysql://localhost:3306/gym_manager";
+        Connection conn = DriverManager.getConnection(dbUrl, "root", "Midori10");// TODO : to change from root!!!
+        conn.setAutoCommit(false); // autocommit : met à jour la db automatiquement
+
+        Statement stmt = conn.createStatement();
+
+
+//        String sql = "insert into user (id, name) values (?, ?)";
+//        PreparedStatement insertStmt = conn.prepareStatement(sql);
+//
+//        for (int i = 0; i < ids.length; i++) {
+//            insertStmt.setInt(1, ids[i]);
+//            insertStmt.setString(2, names[i]);
+//
+//            insertStmt.executeUpdate();
+//        }
+//        conn.commit(); // on écrit les données permanentes
+//        insertStmt.close();
+
+        String sqlPrint = "select * from user";
+        ResultSet rs = stmt.executeQuery(sqlPrint);
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            System.out.println(id + " " + name);
+        }
+
+//        stmt.execute("drop table user"); // on efface les données.
+        conn.commit();
+        stmt.close();
+        conn.close();
 
     }
 }
